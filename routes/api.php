c90a14422a39e210b564c3442e0f689fef74146b
Debkumar <debkumar.daschakraborty@techprostudio.com>
2018-09-12 20:42:12 +0530

    Modify Routes

--- routes/api.php
+++ routes/api.php
@@ -6,8 +6,8 @@ $api = app('Dingo\Api\Routing\Router');
 
 // header  Accept:application/vnd.lumen.v1+json
 $api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Api\V1',
-    'middleware' => []
+    'namespace'  => 'App\Http\Controllers\Api\V1',
+    'middleware' => [],
 ], function ($api) {
     // test
     $api->get('test', 'TestController@test');
@@ -17,95 +17,125 @@ $api->version('v1', [
     ], function ($api) {
         // register
         $api->post('register', [
-            'as' => 'authorize.register',
+            'as'   => 'authorize.register',
             'uses' => 'AuthController@register',
         ]);
         $api->post('register_test', [
-            'as' => 'authorize.register_test',
+            'as'   => 'authorize.register_test',
             'uses' => 'AuthController@register_test',
         ]);
 
         // login
         $api->post('login', [
-            'as' => 'authorize.login',
+            'as'   => 'authorize.login',
             'uses' => 'AuthController@login',
         ]);
 
         // refresh
         $api->post('refresh', [
-            'as' => 'authorize.refresh',
+            'as'   => 'authorize.refresh',
             'uses' => 'AuthController@refresh',
         ]);
 
         // me
         $api->get('me', [
-            'as' => 'authorize.me',
+            'as'   => 'authorize.me',
             'uses' => 'AuthController@me',
         ]);
 
         // getCurrentToken
         $api->post('token', [
-            'as' => 'authorize.getCurrentToken',
+            'as'   => 'authorize.getCurrentToken',
             'uses' => 'AuthController@getCurrentToken',
         ]);
 
         // logout
         $api->post('logout', [
-            'as' => 'authorize.logout',
+            'as'   => 'authorize.logout',
             'uses' => 'AuthController@logout',
         ]);
     });
 
     // need authentication
     $api->group([
-        //'middleware' => 'api.auth'
+        'middleware' => 'api.auth',
     ], function ($api) {
-        // User
-        // my detail
+        // RESTful API
+        $api->resource('/post', 'PostController', [
+            'only' => [
+            ],
+        ]);
+        $api->resource('/programme', 'ProgrammeController', [
+            'except' => [
+                'update',
+                'destroy',
+            ],
+        ]);
+        $api->resource('/programme-participant', 'ProgrammeParticipantController', [
+            'only' => [
+                'store',
+                'show',
+            ],
+        ]);
+        $api->resource('/programme-classformat', 'ProgrammeClassFormatController', [
+            'only' => [
+                'store',
+                'show',
+            ],
+        ]);
+        $api->resource('/student', 'StudentController', [
+            'only' => [
+            ],
+        ]);
+        $api->resource('/teacher', 'TeacherController', [
+            'only' => [
+            ],
+        ]);
+
+        // Custom RESTful API
+        // My Details
+        $api->get('user', [
+            'as'   => 'user.index',
+            'uses' => 'UserController@index',
+        ]);
         // $api->get('user', [
         //     'as' => 'user.show',
         //     'uses' => 'UserController@show'
         // ]);
-        $api->get('user', [
-            'as' => 'user.index',
-            'uses' => 'UserController@index'
+        // Show class under a programme
+        $api->get('programme/classes/{programme_id}', [
+            'as'   => 'programme.show-class',
+            'uses' => 'ProgrammeController@show_class',
         ]);
-        // update info
-        $api->patch('user', [
-            'as' => 'users.update',
-            'uses' => 'UserController@patch'
+        // Show module under a programme
+        $api->get('programme/modules/{programme_id}/{class_id}', [
+            'as'   => 'programme.show-module',
+            'uses' => 'ProgrammeController@show_module',
+        ]);
+        // Store file under a programme create
+        $api->post('programme/files', [
+            'as'   => 'programme.store-file',
+            'uses' => 'ProgrammeController@store_file',
+        ]);
+        // Store class under a programme
+        $api->post('programme/classes', [
+            'as'   => 'programme.store-class',
+            'uses' => 'ProgrammeController@store_class',
         ]);
         // edit password
         $api->put('user/password', [
-            'as' => 'user.edit.password',
-            'uses' => 'UserController@editPassword'
+            'as'   => 'user.edit.password',
+            'uses' => 'UserController@editPassword',
         ]);
-
-        // RESTful
-        $api->resource('/post', 'PostController');
-        $api->resource('/programme', 'ProgrammeController');
-        $api->resource('/programme-participant', 'ProgrammeParticipantController');
-        $api->resource('/programme-classformat', 'ProgrammeClassFormatController');
-        //store_file
-        $api->get('programme/classes/{programme_id}', [
-            'as' => 'programme.show-class',
-            'uses' => 'ProgrammeController@show_class'
+        // update info
+        $api->patch('user', [
+            'as'   => 'users.update',
+            'uses' => 'UserController@patch',
         ]);
-        $api->get('programme/modules/{programme_id}/{class_id}', [
-            'as' => 'programme.show-module',
-            'uses' => 'ProgrammeController@show_module'
-        ]);
-        $api->post('programme/files', [
-            'as' => 'programme.store-file',
-            'uses' => 'ProgrammeController@store_file'
-        ]);
-        $api->post('programme/classes', [
-            'as' => 'programme.store-class',
-            'uses' => 'ProgrammeController@store_class'
-        ]);
+        // Delete file under a programme create
         $api->delete('programme/files/{files}', [
-            'as' => 'programme.destroy-file',
-            'uses' => 'ProgrammeController@destroy_file'
+            'as'   => 'programme.destroy-file',
+            'uses' => 'ProgrammeController@destroy_file',
         ]);
     });
 
@@ -114,9 +144,8 @@ $api->version('v1', [
 // v2  version API
 // header  Accept:application/vnd.lumen.v2+json
 $api->version('v2', [
-    'namespace' => 'App\Http\Controllers\Api\V2'
+    'namespace' => 'App\Http\Controllers\Api\V2',
 ], function ($api) {
-
     // test
     $api->get('test', 'TestController@test');
 
